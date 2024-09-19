ea27c3622a9146923d09337ff076526e34aabeae
Julia Kulla-Mader <64906+juliakm@users.noreply.github.com>
2024-09-19 14:48:20 +0000

    Adding CSS update for spacing of chat container

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -842,7 +842,7 @@ const Chat = () => {
               <Stack className={styles.chatEmptyState}>
                 <img src={logo} className={styles.chatIcon} aria-hidden="true" />
                 <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
-                <HtmlFromText htmlText={ui?.chat_description!}/>
+                <p><HtmlFromText htmlText={ui?.chat_description!}/></p>
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
